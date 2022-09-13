56e0ac5dd0df2df17d4dfaf808b485ddcd0ffd8e
universal developer <69350489+universal-developer@users.noreply.github.com>
2022-09-13 14:54:29 +0200

    Added background video and so on

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -21,10 +21,10 @@ const Sidebar = ({ isOpen, toggle }) => {
 
         <SidebarWrapper>
           <SidebarMenu>
-            <SidebarLink to='about'>About</SidebarLink>
-            <SidebarLink to='discover'>Discover</SidebarLink>
-            <SidebarLink to='services'>Services</SidebarLink>
-            <SidebarLink to='signup'>Sign Up</SidebarLink>
+            <SidebarLink to='about' onClick={toggle}>About</SidebarLink>
+            <SidebarLink to='discover' onClick={toggle}>Discover</SidebarLink>
+            <SidebarLink to='services' onClick={toggle}>Services</SidebarLink>
+            <SidebarLink to='signup' onClick={toggle}>Sign Up</SidebarLink>
           </SidebarMenu>
         </SidebarWrapper>
 
